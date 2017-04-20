5a37f4c563a51939a0c124fc6022e7d1dd55cee0
johan <johan.schantz@telia.com>
2017-04-20 18:00:22 +0200

    added response message

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,18 +4,16 @@ angular.module('building-blocks.controllers', [])
   $scope.news = News.query();
 })
 
-.controller('HelpRequestController', function($scope, HelpRequest) {
+.controller('HelpRequestController', function($scope, $location, HelpRequest) {
   $scope.error = null;
   $scope.help_request = {};
 
 
   $scope.createHelpRequest = function() {
     HelpRequest.save($scope.help_request, function(response){
-      console.log(response);
+      $scope.error = null;
       $scope.message = response.message;
-      // Move on to another page or whatever you want to do
     }, function(error){
-      // This error handler needs to be refactored before PR. This is cruede
       $scope.error = error.data.message;
     });
   }
